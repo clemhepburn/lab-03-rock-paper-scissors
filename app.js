3c80aa5088b86d14666ad77067883810af985627
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-03-25 15:41:39 -0700

    create html, init state, document elements in js, event listener, randomthrow

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 // import functions and grab DOM elements
-
+const rockGuess = document.querySelector('rock');
+const paperGuess = document.querySelector('paper');
+const scissorsGuess = document.querySelector('scissors');
+const goButton = document.getElementById('throw');
+const resetButton = document.getElementById('reset');
+const winDiv = document.getElementById('wins');
+const loseDiv = document.getElementById('losses');
+const totalDiv = document.getElementById('total');
 // initialize state
+let guesses = 0;
+let wins = 0;
+let losses = 0;
+let total = 0;
 
 // set event listeners to update state and DOM
+goButton.addEventListener('click'() => {
+
+})
